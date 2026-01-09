a2b94c348d12b3d169905864e025e974a15d738a
crystalrealm-maker <chicworld@hotmail.com>
2026-01-09 12:49:03 +0000

    FIX CRÍTICO: Remove flex justify-center que impedia navegação de renderizar 5 itens

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -9,16 +9,13 @@ export default function TabsLayout({
 }) {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black">
-      {/* CONTEÚDO PRINCIPAL COM ESPAÇO PARA NAVIGATION */}
       <div className="pb-20">
         {children}
       </div>
       
-      {/* NAVIGATION FIXO NO FUNDO - CENTRADO COM FLEX */}
+      {/* NAVIGATION FIXO NO FUNDO - LARGURA TOTAL */}
       <div className="fixed bottom-0 left-0 right-0 bg-gray-900 border-t border-gray-700 py-2 z-50">
-        <div className="flex justify-center">
-          <Navigation />
-        </div>
+        <Navigation /> {/* ⬅️ REMOVI a div com flex justify-center */}
       </div>
     </div>
   );
